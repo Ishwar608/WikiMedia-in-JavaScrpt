31356b5f05f7c0d8bf314bd2677b554054939e2a
Ishwar <whitehacker608@gmail.com>
2022-12-30 12:35:27 +0530

    media query

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ getData = () => {
     fetch("https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=20&format=json&origin=*&srsearch=" + txt)
         .then(y => y.json())
         .then(y => display(y))
-        
+
 }
 
-display  = (data) =>{
+display = (data) => {
 
     for (const key in data) {
         if (key == "query") {
@@ -19,9 +19,9 @@ display  = (data) =>{
         }
     }
 
-document.getElementById("result").innerHTML = myJson.map((value) => {
-    return ` <div class="item">
+    document.getElementById("result").innerHTML = myJson.map((value) => {
+        return ` <div class="item">
                 <h3 id="title">${value.title}</h3>
     <p>${value.snippet}</p></div>`;
-}).join("");
+    }).join("");
 }
